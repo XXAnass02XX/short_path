9d981398ef8b084df4e0e47c71a7f447b0c8e547
anass haydar <anasshaydar2002@hotmail.com>
2023-08-27 22:06:33 +0200

    diagonals on off , gotta fix a_start algo

--- code_1.py
+++ code_1.py
@@ -64,7 +64,7 @@ def fill_window(grid):
     window.blit(text_surface, (5.5*cells_len-4,HEIGHT -2*cells_len + cells_len//2+2))
     pygame.display.update()
 
-def reset():
+def reset(diagonals):
     for line in tab:
         for cell in line:
             cell.is_end = False
@@ -73,7 +73,10 @@ def reset():
             cell.is_path = False
             cell.is_visited = False
             cell.parent = None
-            cell.possible_next = ['r','l','u','d','ur','ul','dr','dl']
+            if diagonals:
+                cell.possible_next = ['r','l','u','d','ur','ul','dr','dl']
+            else:
+                cell.possible_next = ['r','l','u','d']
 
 
 def in_grid(a):
@@ -83,61 +86,93 @@ def in_grid(a):
         return True
     return False
 
-def change_to_path(i,j):
+def change_to_path(i,j,diagonals):
     '''giving the coordinates of a wall cell switching it to a path'''
     cell = tab[j][i]
     if cell.is_wall == True and cell.is_start == False and cell.is_end == False:
         cell.is_wall = False
-        to_change = [(i-1,j-1),(i,j-1),(i+1,j-1),(i-1,j),(i+1,j),(i-1,j+1),(i,j+1),(i+1,j+1)]
-        s=0
-        for elt in to_change:
-            if in_grid(elt):
-                if s==0:
-                    tab[elt[1]][elt[0]].possible_next.append('dr')
-                elif s==1:
-                    tab[elt[1]][elt[0]].possible_next.append('d')
-                elif s==2:
-                    tab[elt[1]][elt[0]].possible_next.append('dl')
-                elif s==3:
-                    tab[elt[1]][elt[0]].possible_next.append('r')
-                elif s==4:
-                    tab[elt[1]][elt[0]].possible_next.append('l')
-                elif s==5:
-                    tab[elt[1]][elt[0]].possible_next.append('ur')
-                elif s==6:
-                    tab[elt[1]][elt[0]].possible_next.append('u')
-                else:
-                    tab[elt[1]][elt[0]].possible_next.append('ul')
-            s+=1
+        if diagonals:
+            to_change = [(i-1,j-1),(i,j-1),(i+1,j-1),(i-1,j),(i+1,j),(i-1,j+1),(i,j+1),(i+1,j+1)]
+            s=0
+            for elt in to_change:
+                if in_grid(elt):
+                    if s==0:
+                        tab[elt[1]][elt[0]].possible_next.append('dr')
+                    elif s==1:
+                        tab[elt[1]][elt[0]].possible_next.append('d')
+                    elif s==2:
+                        tab[elt[1]][elt[0]].possible_next.append('dl')
+                    elif s==3:
+                        tab[elt[1]][elt[0]].possible_next.append('r')
+                    elif s==4:
+                        tab[elt[1]][elt[0]].possible_next.append('l')
+                    elif s==5:
+                        tab[elt[1]][elt[0]].possible_next.append('ur')
+                    elif s==6:
+                        tab[elt[1]][elt[0]].possible_next.append('u')
+                    else:
+                        tab[elt[1]][elt[0]].possible_next.append('ul')
+                s+=1
+        else:
+            to_change = [(i,j-1),(i-1,j),(i+1,j),(i,j+1)]
+            s=0
+            for elt in to_change:
+                if in_grid(elt):
+                    if s==0:
+                        tab[elt[1]][elt[0]].possible_next.append('d')
+                    elif s==1:
+                        tab[elt[1]][elt[0]].possible_next.append('r')
+                    elif s==2:
+                        tab[elt[1]][elt[0]].possible_next.append('l')
+                    else:
+                        tab[elt[1]][elt[0]].possible_next.append('u')
+                s+=1
+
         pygame.draw.rect(window, (255, 255, 255), (i*cells_len+2, j*cells_len+2, cells_len-4, cells_len-4))
         pygame.display.update()
 
-def change_to_wall(i,j):
+def change_to_wall(i,j,diagonals):
     '''switching the cell with the (i,j) coordinates to a wall'''
     cell_a = tab[j][i]
     if cell_a.is_wall == False and cell_a.is_start == False and cell_a.is_end == False:
         tab[j][i].is_wall = True
-        to_change = [(i-1,j-1),(i,j-1),(i+1,j-1),(i-1,j),(i+1,j),(i-1,j+1),(i,j+1),(i+1,j+1)]
-        s=0
-        for elt in to_change:
-            if in_grid(elt):
-                if s==0:
-                    tab[elt[1]][elt[0]].possible_next.remove('dr')
-                elif s==1:
-                    tab[elt[1]][elt[0]].possible_next.remove('d')
-                elif s==2:
-                    tab[elt[1]][elt[0]].possible_next.remove('dl')
-                elif s==3:
-                    tab[elt[1]][elt[0]].possible_next.remove('r')
-                elif s==4:
-                    tab[elt[1]][elt[0]].possible_next.remove('l')
-                elif s==5:
-                    tab[elt[1]][elt[0]].possible_next.remove('ur')
-                elif s==6:
-                    tab[elt[1]][elt[0]].possible_next.remove('u')
-                else:
-                    tab[elt[1]][elt[0]].possible_next.remove('ul')
-            s+=1
+        if diagonals:
+            to_change = [(i-1,j-1),(i,j-1),(i+1,j-1),(i-1,j),(i+1,j),(i-1,j+1),(i,j+1),(i+1,j+1)]
+            s=0
+            for elt in to_change:
+                if in_grid(elt):
+                    if s==0:
+                        tab[elt[1]][elt[0]].possible_next.remove('dr')
+                    elif s==1:
+                        tab[elt[1]][elt[0]].possible_next.remove('d')
+                    elif s==2:
+                        tab[elt[1]][elt[0]].possible_next.remove('dl')
+                    elif s==3:
+                        tab[elt[1]][elt[0]].possible_next.remove('r')
+                    elif s==4:
+                        tab[elt[1]][elt[0]].possible_next.remove('l')
+                    elif s==5:
+                        tab[elt[1]][elt[0]].possible_next.remove('ur')
+                    elif s==6:
+                        tab[elt[1]][elt[0]].possible_next.remove('u')
+                    else:
+                        tab[elt[1]][elt[0]].possible_next.remove('ul')
+                s+=1
+        else:
+            to_change = [(i,j-1),(i-1,j),(i+1,j),(i,j+1)]
+            s=0
+            print('gd1')
+            for elt in to_change:
+                if in_grid(elt):
+                    if s==0:
+                        tab[elt[1]][elt[0]].possible_next.remove('d')
+                    elif s==1:
+                        tab[elt[1]][elt[0]].possible_next.remove('r')
+                    elif s==2:
+                        tab[elt[1]][elt[0]].possible_next.remove('l')
+                    else:
+                        tab[elt[1]][elt[0]].possible_next.remove('u')
+                s+=1
         pygame.draw.rect(window, (0, 0, 0), (i*cells_len+2, j*cells_len+2, cells_len-4, cells_len-4))
         pygame.display.update()
 
@@ -145,7 +180,7 @@ def get_cord(cell):
     '''given a cell this funct returns the coordinates of this cell'''
     return (cell.x,cell.y)
 
-def get_next(a):
+def get_next(a,diagonals):
     '''given a cell this function returns a list of possible next cells'''
     l=[]
     for elt in a.possible_next:
@@ -159,29 +194,31 @@ def get_next(a):
         elif elt=='r':
             if in_grid((x+1,y)):
                 l.append(tab[y][x+1])
-        elif elt=='ur':
-            if in_grid((x+1,y-1)):
-                l.append(tab[y-1][x+1])
-        elif elt=='ul':
-            if in_grid((x-1,y-1)):
-                l.append(tab[y-1][x-1])
-        elif elt=='dr':
-            if in_grid((x+1,y+1)):
-                l.append(tab[y+1][x+1])
-        elif elt=='dl':
-            if in_grid((x-1,y+1)):
-                l.append(tab[y+1][x-1])
-        else:
+        elif elt == 'l':
             if in_grid((x-1,y)):
                 l.append(tab[y][x-1])
+        elif diagonals:
+            if elt=='ur':
+                if in_grid((x+1,y-1)):
+                    l.append(tab[y-1][x+1])
+            elif elt=='ul':
+                if in_grid((x-1,y-1)):
+                    l.append(tab[y-1][x-1])
+            elif elt=='dr':
+                if in_grid((x+1,y+1)):
+                    l.append(tab[y+1][x+1])
+            elif elt=='dl':
+                if in_grid((x-1,y+1)):
+                    l.append(tab[y+1][x-1])
+        
     return l
 
 def get_distance(coord1,coord2):
     '''calculate the ditance between two cells of coors 1 and 2 '''
     return sqrt((coord1[0]-coord2[0])**2+(coord1[1]-coord2[1])**2)
-def get_nearest_neighbor(cell,dic):
+def get_nearest_neighbor(cell,dic,diagonals):
     '''given a cell this function returns the neighbor cell with the lowest distance from the start'''
-    l = get_next(cell)
+    l = get_next(cell,diagonals)
     a = l[0]
     b = dic[get_cord(l[0])]
     for elt in l[1:]:
@@ -191,7 +228,7 @@ def get_nearest_neighbor(cell,dic):
             b = d
     return a
 
-def dijkstra_animation(start_end):
+def dijkstra_animation(start_end,diagonals):
     cells_dist = {(x, y): float('inf') for y in range(HEIGHT) for x in range(WIDTH)}
     cells_dist[get_cord(start_end[0])] = 0
     current = start_end[0]
@@ -199,7 +236,7 @@ def dijkstra_animation(start_end):
     visited = []
     end_is_found = False
     while start_end[1] not in visited :
-        next_cells = get_next(current)
+        next_cells = get_next(current,diagonals)
         for elt in next_cells:
             if elt == start_end[1]:
                 end_is_found= True
@@ -226,12 +263,11 @@ def dijkstra_animation(start_end):
         current_coord = min(UnvisitedCellWithFiniteDistance, key=lambda k: UnvisitedCellWithFiniteDistance[k])
         current = tab[current_coord[1]][current_coord[0]]
         pygame.display.update()
-    print(end_is_found)
     if end_is_found:
         current = start_end[1]
         while current is not start_end[0]:
             '''draw the shortest path starting from the end to the start'''
-            current = get_nearest_neighbor(current,cells_dist)
+            current = get_nearest_neighbor(current,cells_dist,diagonals)
             current_coord = get_cord(current)
             tab[current_coord[1]][current_coord[0]].is_path = True
             fill_window(tab)
@@ -250,8 +286,8 @@ def change_to_cost_for_heapq(start_end,cell):
     a = h_cost(start_end,coord)
     return (a,(coord[0],coord[1],a))
 
-def get_neighbor_with_small_h_cost(cell,d):
-    next_list = get_next(cell)
+def get_neighbor_with_small_h_cost(cell,d,diagonals):
+    next_list = get_next(cell,diagonals)
     for elt in next_list:
         coord = get_cord(elt)
         if coord in d:
@@ -266,7 +302,7 @@ def get_neighbor_with_small_h_cost(cell,d):
                 a = d[coord]
     return b
 
-def a_star_animation(start_end):
+def a_star_animation(start_end,diagonals):
     '''given a start point and an end point , this function does the simulation of a start algorithme for shortest path between them'''
     visited = {}
     potentiel_next = {} 
@@ -280,7 +316,7 @@ def a_star_animation(start_end):
         current = min(potentiel_next, key=potentiel_next.get)
         del potentiel_next[current]
         visited[current] = f_cost(start_end,get_cord(current))
-        neighbors = get_next(current)
+        neighbors = get_next(current,diagonals)
         for cell in neighbors:
             if cell not in visited:
                 if cell not in potentiel_next:
@@ -318,15 +354,14 @@ def get_unvisited_neighbors(a,visited):
                 l1.append(elt)
     return l1
 
-def maze_generator():
+def maze_generator(diagonals):
     n,m = WIDTH//cells_len,HEIGHT//cells_len
-    print(n,m)
     for i in range(0,n,2):
         for j in range(0,m-2):
-            change_to_wall(i,j)
+            change_to_wall(i,j,diagonals)
     for j in range(0,m-2,2):
         for i in range(0,n):
-            change_to_wall(i,j)
+            change_to_wall(i,j,diagonals)
     visited = {}
     start = (1,1)
     stack = [start]
@@ -337,7 +372,7 @@ def maze_generator():
         if unv_neighbors:
             neighbor = choice(unv_neighbors)
             m,n = abs(current[0]+neighbor[0])//2,abs(current[1]+neighbor[1])//2
-            change_to_path(m,n)
+            change_to_path(m,n,diagonals)
             visited[neighbor]=True
             stack.append(current)
             stack.append(neighbor)
@@ -351,10 +386,37 @@ def maze_generator():
                     break
         pygame.display.update()
 
+def diagonalize(diagonals):
+    if not diagonals:
+        print('in2')
+        for line in tab:
+            for cell in line:
+                if 'ul' in cell.possible_next:
+                    cell.possible_next.remove('ul')
+                elif 'ur' in cell.possible_next:
+                    cell.possible_next.remove('ur')
+                elif 'dl' in cell.possible_next:
+                    cell.possible_next.remove('dl')
+                elif 'dr' in cell.possible_next:
+                    cell.possible_next.remove('dr')
+    else:
+        for line in tab:
+            for cell in line:
+                if 'ul' not in cell.possible_next:
+                    cell.possible_next.append('ul')
+                elif 'ur' not in cell.possible_next:
+                    cell.possible_next.append('ur')
+                elif 'dl' not in cell.possible_next:
+                    cell.possible_next.append('dl')
+                elif 'dr' not in cell.possible_next:
+                    cell.possible_next.append('dr')
+
+
 def main():
     start_end = []
     '''main fonc that displays the program window'''
     running= True
+    diagonals = True
     while running:
         fill_window(tab)
         event = pygame.event.poll()
@@ -366,7 +428,7 @@ def main():
             i,j = x//cells_len , y//cells_len
             specific_key = pygame.key.get_pressed()[pygame.K_w]
             if specific_key:
-                change_to_path(i,j)
+                change_to_path(i,j,diagonals)
                 while pygame.key.get_pressed()[pygame.K_w] and pygame.mouse.get_pressed(num_buttons=3)[0]:
                     x, y = pygame.mouse.get_pos()
                     i,j = x//cells_len , y//cells_len
@@ -375,9 +437,9 @@ def main():
                         break
                     if j<len(tab):
                         if in_grid((i,j)):
-                            change_to_path(i,j)
+                            change_to_path(i,j,diagonals)
             elif j<len(tab):
-                change_to_wall(i,j)
+                change_to_wall(i,j,diagonals)
                 while pygame.mouse.get_pressed(num_buttons=3)[0]:
                     x, y = pygame.mouse.get_pos()
                     i,j = x//cells_len , y//cells_len
@@ -386,7 +448,7 @@ def main():
                         break
                     if j<len(tab):
                         if in_grid((i,j)):
-                            change_to_wall(i,j)
+                            change_to_wall(i,j,diagonals)
             elif len(start_end) ==2 and cells_len+2<=x<=cells_len+2 + 3* cells_len and HEIGHT -2*cells_len + cells_len//2+2 <=y <=HEIGHT -2*cells_len + cells_len//2+2 + cells_len:
                 while pygame.mouse.get_pressed(num_buttons=3)[0]:
                     event = pygame.event.poll()
@@ -395,7 +457,7 @@ def main():
                 x, y = pygame.mouse.get_pos()
                 i,j = x//cells_len , y//cells_len
                 if cells_len+2<=x<=cells_len+2 + 3* cells_len and HEIGHT -2*cells_len + cells_len//2+2 <=y <=HEIGHT -2*cells_len + cells_len//2+2 + cells_len:
-                    dijkstra_animation(start_end)
+                    dijkstra_animation(start_end,diagonals)
             elif len(start_end) ==2 and 5*cells_len+2<=x<=5*cells_len+2+cells_len*3-4 and HEIGHT -2*cells_len + cells_len//2+2 <=y <=HEIGHT -2*cells_len + cells_len//2+2 + cells_len:
                 while pygame.mouse.get_pressed(num_buttons=3)[0]:
                     event = pygame.event.poll()
@@ -404,7 +466,7 @@ def main():
                 x, y = pygame.mouse.get_pos()
                 i,j = x//cells_len , y//cells_len
                 if 5*cells_len+2<=x<=5*cells_len+2+cells_len*3-4 and HEIGHT -2*cells_len + cells_len//2+2 <=y <=HEIGHT -2*cells_len + cells_len//2+2 + cells_len:
-                    a_star_animation(start_end)
+                    a_star_animation(start_end,diagonals)
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
             '''right click'''
             x, y = pygame.mouse.get_pos()
@@ -419,10 +481,14 @@ def main():
                         start_end.append(tab[j][i])
         elif event.type == pygame.KEYDOWN:
             if event.unicode.lower() == 'm':
-                maze_generator()
+                maze_generator(diagonals)
             if event.unicode.lower() == 'r':
-                reset()
+                reset(diagonals)
                 start_end=[]
+            if event.unicode.lower() == 'd':
+                diagonals = not diagonals
+                print('in1',diagonals)
+                diagonalize(diagonals)
     pygame.quit()
 
 if __name__ == '__main__':
